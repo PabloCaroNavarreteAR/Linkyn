efbd5a4de0e63788238f737cf843407a744848bd
Pablo Caro <p.caronavarrete@gmail.com>
2020-02-21 16:37:06 -0300

    Maquetado desktop

--- js/functions.js
+++ js/functions.js
@@ -12,6 +12,15 @@ document.addEventListener('DOMContentLoaded', () =>{
 		delay: (el, i) => 10 * i,
 		offset: '-*50',
 	})
+})
+
+document.addEventListener('DOMContentLoaded', () =>{
+	//console.log('works');
+	//animaciones iniciales
+	anime.timeline({
+		easing: 'easeOutExpo',
+		
+	})
 	.add({
 		targets: '.project-page',
 		opacity:[0,1],
@@ -27,6 +36,92 @@ document.addEventListener('DOMContentLoaded', () =>{
 		offset: '-*0',
 	})
 })
+
+document.addEventListener('DOMContentLoaded', () =>{
+	//console.log('works');
+	//animaciones iniciales
+	anime.timeline({
+		easing: 'easeOutExpo',
+		
+	})
+	.add({
+		targets: '.project-detail-page',
+		opacity:[0,1],
+		translateX:[-500,0],
+		delay: (el, i) => 10 * i,
+		offset: '-*50',
+	})
+	.add({
+		targets: '.project-detail-page .carousel-caption',
+		opacity:[0,1],
+		translateX:[+100,0],
+		delay: (el, i) => 10 * i,
+		offset: '-*0',
+	})
+})
+
+
+
+document.addEventListener('DOMContentLoaded', () =>{
+	//console.log('works');
+	//animaciones iniciales
+	anime.timeline({
+		easing: 'easeOutExpo',
+		
+	})
+	.add({
+		targets: '.contact-page',
+		opacity:[0,1],
+		translateX:[-500,0],
+		delay: (el, i) => 0 * i,
+		offset: '-*50',
+	})
+	.add({
+		targets: '.contact-page .carousel-caption',
+		opacity:[0,1],
+		translateX:[-100,0],
+		delay: (el, i) => 0 * i,
+		offset: '-*0',
+	})
+	.add({
+		targets: '.contact-page .navbar',
+		opacity:[0,1],
+		translateY:[-500,0],
+		delay: (el, i) => 0 * i,
+		offset: '-*50',
+	})
+})
+
+document.addEventListener('DOMContentLoaded', () =>{
+	//console.log('works');
+	//animaciones iniciales
+	anime.timeline({
+		easing: 'easeOutExpo',
+		
+	})
+	.add({
+		targets: '.prensa-page',
+		opacity:[0,1],
+		translateX:[-500,0],
+		delay: (el, i) => 0 * i,
+		offset: '-*50',
+	})
+	.add({
+		targets: '.prensa-page .carousel-caption',
+		opacity:[0,1],
+		translateY:[-500,0],
+		delay: (el, i) => 0 * i,
+		offset: '-*0',
+	})
+	.add({
+		targets: '.prensa-page .navbar',
+		opacity:[0,1],
+		translateY:[-500,0],
+		delay: (el, i) => 0 * i,
+		offset: '-*50',
+	})
+})
+
 var proyectos = document.getElementById("btnProyectos");
 proyectos.addEventListener('click', () => {
   anime.timeline({
